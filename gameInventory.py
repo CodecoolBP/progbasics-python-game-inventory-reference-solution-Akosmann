21f285d6d7050549b5e1116608f747d34cc20fd6
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-11-28 12:36:45 +0000

    Reference solution

--- gameInventory.py
+++ gameInventory.py
@@ -1,38 +1,103 @@
-# This is the file where you must work. Write code in the functions, create new functions, 
-# so they work according to the specification
+def get_total_count(inventory):
+    return sum(inventory.values())
 
-# Displays the inventory.
+
+def get_max_column_widths(inventory):
+    max_name_width = None
+    max_count_width = None
+    for name, count in inventory.items():
+        name_width = len(name)
+        count_width = len(str(count))
+
+        if max_name_width == None or max_name_width < name_width:
+            max_name_width = name_width
+
+        if max_count_width == None or max_count_width < count_width:
+            max_count_width = count_width
+    return (max_name_width, max_count_width)
+
+
 def display_inventory(inventory):
-    pass
+    print('Inventory:')
+    for name, count in inventory.items():
+        print(count, name)
+    total_count = get_total_count(inventory)
+    print('Total number of items: {}'.format(total_count))
 
 
-# Adds to the inventory dictionary a list of items from added_items.
 def add_to_inventory(inventory, added_items):
-    pass
+    for added_item in added_items:
+        if added_item in inventory:
+            inventory[added_item] += 1
+        else:
+            inventory[added_item] = 1
 
-
-# Takes your inventory and displays it in a well-organized table with 
-# each column right-justified. The input argument is an order parameter (string)
-# which works as the following:
-# - None (by default) means the table is unordered
-# - "count,desc" means the table is ordered by count (of items in the inventory) 
-#   in descending order
-# - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
-    pass
+    max_name_width, max_count_width = get_max_column_widths(inventory)
+
+    name_label = 'item name'
+    count_label = 'count'
+
+    name_width = max(max_name_width, len(name_label))
+    count_width = max(max_count_width, len(count_label))
+
+    # Ordering
+    inventory_list = list(inventory.items())
+    if order != None and order.startswith('count'):
+        reverse = order.endswith('desc')
+        inventory_list.sort(key=lambda item: (item[1], item[0]), reverse=reverse)
+
+    # Header
+    print('Inventory:')
+    print('  {}    {}'.format(
+        count_label.center(count_width),
+        name_label.center(name_width)))
+
+    # Separator
+    print('--{}----{}'.format('-' * count_width, '-' * name_width))
+
+    # Rows
+    for name, count in inventory_list:
+        print('  {}    {}'.format(
+            str(count).rjust(count_width),
+            name.rjust(name_width)))
+
+    # Separator
+    print('--{}----{}'.format('-' * count_width, '-' * name_width))
+    print('Total number of items: {}'.format(get_total_count(inventory)))
 
 
-# Imports new inventory items from a file
-# The filename comes as an argument, but by default it's 
-# "import_inventory.csv". The import automatically merges items by name.
-# The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename="import_inventory.csv"):
-    pass
+    with open(filename) as f:
+        line = f.read()
+        items = line.split(',')
+        add_to_inventory(inventory, items)
 
 
-# Exports the inventory into a .csv file.
-# if the filename argument is None it creates and overwrites a file
-# called "export_inventory.csv". The file format is the same plain text 
-# with comma separated values (CSV).
 def export_inventory(inventory, filename="export_inventory.csv"):
-    pass
+    with open(filename, 'w') as f:
+        total_count = get_total_count(inventory)
+        current_count = 0
+        for name, count in inventory.items():
+            for _ in range(count):
+                f.write(name)
+                current_count += 1
+                if not current_count == total_count:
+                    f.write(',')
+
+
+def main():
+    my_inventory = {'cat': 1, 'dog': 2, 'bat': 3}
+    display_inventory(my_inventory)
+    add_to_inventory(my_inventory, ['eel', 'eel'])
+    display_inventory(my_inventory)
+    import_inventory(my_inventory, filename='test_inventory.csv')
+    display_inventory(my_inventory)
+    export_inventory(my_inventory, filename='my_inventory.csv')
+    print_table(my_inventory)
+    print_table(my_inventory, 'count,asc')
+    print_table(my_inventory, 'count,desc')
+
+
+if __name__ == '__main__':
+    main()
